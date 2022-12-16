0a8d60c022378c09e3e3a251f7ab46051cc58734
Cagatay Gevrek <cagataygevrek@gmail.com>
2022-12-16 18:31:53 +0300

    dosya yüklendi.

---
+++ src/veri.js
@@ -0,0 +1,23 @@
+export const sliderFotograflari = [
+  {
+    id: 1,
+    img: "https://i.hizliresim.com/mli2qi3.jpg",
+    title: "Iphone 14",
+    desc: "iPhone 14'ün ekranı yuvarlatılmış köşelere sahiptir. Telefonun kıvrımlı zarif tasarımını takip eden bu köşeler, standart bir dikdörtgenin sınırları içinde yer alır. Standart bir dikdörtgen olarak ölçüldüğünde ekran diyagonal olarak 6.06 inçtir",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://i.hizliresim.com/6q5n6xy.jpg",
+    title: "Samsung Xpress C430W Wireless Color Laser Printer with",
+    desc: "Samsung'un ileri düzey NFC teknolojisi ile mobil baskı daha kolay olamazdı. Samsung'un ücretsiz Mobil Yazdırma Uygulaması sayesinde çıktılarınızı kolayca alabilirsiniz.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://i.hizliresim.com/6jaaqvl.jpg",
+    title: "Asus Vivobook K513EP-L1576",
+    desc: "Çarpıcı renkler ve renkli Enter tuşuyla ASUS VivoBook 15, günlük bilgisayar kullanımına yetenek ve dinamizm katar. Intel® Core™ i5 işlemcisi ve en yeni NVIDIA GeForce GPU'su ile VivoBook 15, işleri halletmek için ihtiyacınız olan gücü sağlar. Ayrıca, bir SSD'nin yüksek veri hızlarını bir HDD'nin büyük kapasiteleriyle birleştiren çift depolama tasarımına sahiptir ve üretkenliği artırmak için size her iki dünyanın da en iyisini sunar.",
+    bg: "fbf0f4",
+  },
+];
