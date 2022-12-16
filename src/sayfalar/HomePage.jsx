74650153575129e08d5ee6dad2688fac3869c417
Cagatay Gevrek <cagataygevrek@gmail.com>
2022-12-16 17:58:05 +0300

    slider import edildi.

--- src/sayfalar/HomePage.jsx
+++ src/sayfalar/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Duyurular from "../bilesenler/Duyurular";
 import Navbar from "../bilesenler/Navbar";
+import Slider from "../bilesenler/Slider";
 
 const HomePage = () => {
   return (
     <div className='Container'>
       <Duyurular />
       <Navbar />
+      <Slider />
     </div>
   );
 };
