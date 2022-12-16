8bee9d11f9002415274aef14f600c45d6320a78f
Cagatay Gevrek <cagataygevrek@gmail.com>
2022-12-16 14:58:42 +0300

    duyurular componenti homepage'e eklendi.

--- src/sayfalar/HomePage.jsx
+++ src/sayfalar/HomePage.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Duyurular from "../bilesenler/Duyurular";
 import Navbar from "../bilesenler/Navbar";
 
 const HomePage = () => {
   return (
     <div className='Container'>
+      <Duyurular />
       <Navbar />
     </div>
   );
