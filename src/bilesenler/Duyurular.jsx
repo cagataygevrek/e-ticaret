3ab322140d17e9f39e02b7dc7a0dad1b911bd805
Cagatay Gevrek <cagataygevrek@gmail.com>
2022-12-16 14:57:40 +0300

    duyurular componenti yüklendi.

---
+++ src/bilesenler/Duyurular.jsx
@@ -0,0 +1,22 @@
+import styled from "@emotion/styled";
+
+const Container = styled.div`
+  height: 30px;
+  background-color: red;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 16x;
+  font-weight: 600;
+`;
+
+const Duyurular = () => {
+  return (
+    <Container>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, expedita!
+    </Container>
+  );
+};
+
+export default Duyurular;
