316bd2d7310433882b99cdff5bbde52870bf215b
Cagatay Gevrek <cagataygevrek@gmail.com>
2022-12-26 15:31:44 +0300

    üye girişi için import yapıldı

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 //import React from "react";
 
-import Urun from "./sayfalar/Urun";
+import GirisYap from "./sayfalar/GirisYap";
+
+//import Urun from "./sayfalar/Urun";
+//import UyeOl from "./sayfalar/UyeOl";
 
 //import UrunlerListesi from "./sayfalar/UrunlerListesi";
 
 //import HomePage from "./sayfalar/HomePage";
 
 const App = () => {
-  return <Urun />;
+  return <GirisYap />;
 };
 
 export default App;
